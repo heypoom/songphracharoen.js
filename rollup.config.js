87c227d26a536419a12262a35d1c8ded1bf71bfa
Phoomparin Mano <phoomparin@gmail.com>
2023-03-24 02:37:04 +0700

    chore: add TypeScript declarations

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,28 @@
 // @ts-check
 
 const typescript = require('@rollup/plugin-typescript').default
+const dts = require('rollup-plugin-dts').default
 
-/** @type {import('rollup').RollupOptions} */
-module.exports = {
-  input: 'src/index.ts',
-  output: [
-    {
-      format: 'umd',
-      file: 'dist/index.js',
-    },
-  ],
-  plugins: [
-    typescript({
-      compilerOptions: {
-        lib: ['es5', 'es6', 'dom'],
-        target: 'es5',
-        importHelpers: false,
+/** @type {import('rollup').RollupOptions[]} */
+module.exports = [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        format: 'umd',
+        file: 'dist/index.js',
       },
-    }),
-  ],
-}
+    ],
+    plugins: [
+      typescript({
+        tsconfig: 'tsconfig.json',
+      }),
+    ],
+  },
+  {
+    // path to your declaration files root
+    input: './dist/dts/index.d.ts',
+    output: [{file: 'dist/index.d.ts', format: 'es'}],
+    plugins: [dts()],
+  },
+]
